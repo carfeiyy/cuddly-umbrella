644a7149c833a8999f14abf0017176fe73b98b9f
carfeiyy <125617834+carfeiyy@users.noreply.github.com>
2023-02-16 19:19:09 -0500

    Create First.py

---
+++ First.py
@@ -0,0 +1,10 @@
+#This is my first code.
+user=input("How could you work?")
+age=input("How old are you?")
+
+age=int(age)
+
+if age>=20 :
+    print("You are cool.")
+else:
+    print("You are young!")
